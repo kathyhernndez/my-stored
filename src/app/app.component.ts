0cb406be869d915ddcde750566f5f8408e4396f6
Katherine Hernandez <katherinehcontact@gmail.com>
2022-04-14 15:53:14 -0400

    creando mi primer app en angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-stored';
+  name = 'Nicolas';
+  age = 18;
+  img = 'https://source.unsplash.com/random';
+  btnDisable = true;
+  person = {
+    name: 'Nicolas',
+    age: 18,
+    avatar: 'https://source.unsplash.com/random'
+  }
+  toogleButton(){
+    this.btnDisable = !this.btnDisable;
+  }
+
+  increaseAge(){
+    this.person.age += 1;
+  }
+
+  onScroll(event: Event){
+    const element = event.target as HTMLElement;
+    console.log(element.scrollTop);
+  }
+  changeName(event: Event){
+    const element = event.target as HTMLInputElement;
+    this.person.name = element.value;
+
+
+  }
 }
---
+++ src/app/recap.ts
@@ -0,0 +1,10 @@
+class Person{
+
+  constructor(public age: number, public lastName: string) {}
+}
+
+const nico = new Person(15, 'Molina');
+
+nico.age;
+nico.lastName;
+
