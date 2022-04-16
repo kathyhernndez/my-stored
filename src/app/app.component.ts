3965549f30933b3ebc5fddf8a5e58ccf31273a36
Katherine Hernandez <katherinehcontact@gmail.com>
2022-04-15 20:31:32 -0400

    ngfor, ngif, ngfor array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+import { Product } from './product.model';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -15,6 +16,42 @@ export class AppComponent {
     age: 18,
     avatar: 'https://source.unsplash.com/random'
   }
+  names: string[] = ['Nico', 'Juli', 'Santi'];
+  newName = "";
+  products: Product[] = [
+    {
+      name: 'EL mejor juguete',
+      price: 565,
+      image: './assets/images/toy.jpg',
+      category: 'all',
+    },
+    {
+      name: 'Bicicleta casi nueva',
+      price: 356,
+      image: './assets/images/bike.jpg'
+    },
+    {
+      name: 'Colleción de albumnes',
+      price: 34,
+      image: './assets/images/album.jpg'
+    },
+    {
+      name: 'Mis libros',
+      price: 23,
+      image: './assets/images/books.jpg'
+    },
+    {
+      name: 'Casa para perro',
+      price: 34,
+      image: './assets/images/house.jpg'
+    },
+    {
+      name: 'Gafas',
+      price: 3434,
+      image: './assets/images/glasses.jpg'
+    }
+  ];
+
   toogleButton(){
     this.btnDisable = !this.btnDisable;
   }
@@ -32,5 +69,13 @@ export class AppComponent {
     this.person.name = element.value;
 
 
+  }
+
+  addName() {
+    this.names.push(this.newName);
+    this.newName = '';
+  }
+  deleteName(index: number){
+    this.names.splice(index, 1);
   }
 }
